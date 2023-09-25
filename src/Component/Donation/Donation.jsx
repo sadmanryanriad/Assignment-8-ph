83379373484d9f581f9642f3e448faeeb1740b40
Sadman Ryan Riad <87647544+sadmanryanriad@users.noreply.github.com>
2023-09-25 11:56:25 +0600

    fixed rounded issue and added see all button to the bottom

--- src/Component/Donation/Donation.jsx
+++ src/Component/Donation/Donation.jsx
@@ -4,10 +4,16 @@ import DonationCard from "../Cards/DonationCard";
 const Donation = () => {
     const cards = useLoaderData();
     return (
-        <div className="mt-20 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
-            <DonationCard cards={cards}></DonationCard>
-            <DonationCard cards={cards}></DonationCard>
-            <DonationCard cards={cards}></DonationCard>
+        <div>
+            <div className="mt-20 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
+                <DonationCard cards={cards}></DonationCard>
+                <DonationCard cards={cards}></DonationCard>
+                <DonationCard cards={cards}></DonationCard>
+                <DonationCard cards={cards}></DonationCard>
+            </div>
+            <div className="flex mt-8">
+            <button className="btn w-28 btn-success mx-auto mb-10">See All</button>
+            </div>
         </div>
     );
 };
