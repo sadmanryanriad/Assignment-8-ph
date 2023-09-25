d1100c665968b202f775a1ed0230320e5d733b1a
Sadman Ryan Riad <87647544+sadmanryanriad@users.noreply.github.com>
2023-09-25 14:49:26 +0600

    made see all button functional

--- src/Component/Donation/Donation.jsx
+++ src/Component/Donation/Donation.jsx
@@ -4,20 +4,23 @@ import { useState } from "react";
 
 const Donation = () => {
     const cards = useLoaderData();
-    const [flag,setFlag] = useState(true);
+    const [flag, setFlag] = useState(true);
 
-    function handleSeeButton(){
+    function handleSeeButton() {
         setFlag(!flag);
     }
 
     return (
         <div>
             <div className="mt-20 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
-                {cards.map(card => <DonationCard key={card.id} card={card}></DonationCard>)}
+                {
+                    flag ? cards.slice(0, 4).map(card => <DonationCard key={card.id} card={card}></DonationCard>) :
+                        cards.map(card => <DonationCard key={card.id} card={card}></DonationCard>)
+                }
 
             </div>
             <div className="flex mt-8">
-            <button onClick={handleSeeButton} className="btn w-28 btn-success mx-auto mb-10 text-white">{flag? 'See All': 'See less'}</button>
+                <button onClick={handleSeeButton} className="btn w-28 btn-success mx-auto mb-10 text-white">{flag ? 'See All' : 'See less'}</button>
             </div>
         </div>
     );
