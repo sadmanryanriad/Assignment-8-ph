5ea8d09e1026596cef47608aab56561661e17982
Sadman Ryan Riad <87647544+sadmanryanriad@users.noreply.github.com>
2023-09-25 14:37:35 +0600

    made see all button dynamic in donation page

--- src/Component/Donation/Donation.jsx
+++ src/Component/Donation/Donation.jsx
@@ -1,8 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import DonationCard from "../Cards/DonationCard";
+import { useState } from "react";
 
 const Donation = () => {
     const cards = useLoaderData();
+    const [flag,setFlag] = useState(true);
+
+    function handleSeeButton(){
+        setFlag(!flag);
+    }
+
     return (
         <div>
             <div className="mt-20 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -10,7 +17,7 @@ const Donation = () => {
 
             </div>
             <div className="flex mt-8">
-            <button className="btn w-28 btn-success mx-auto mb-10 text-white">See All</button>
+            <button onClick={handleSeeButton} className="btn w-28 btn-success mx-auto mb-10 text-white">{flag? 'See All': 'See less'}</button>
             </div>
         </div>
     );
